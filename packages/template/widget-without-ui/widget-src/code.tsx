e1d3235ea244f67b20d787fbdd3fa6c30124c278
dokome <1098265675@qq.com>
2023-01-25 10:53:58 +0800

    feat: 添加项目模板

---
+++ packages/template/widget-without-ui/widget-src/code.tsx
@@ -0,0 +1,22 @@
+const { widget } = jsDesign;
+const { AutoLayout, Ellipse, Frame, Image, Rectangle, SVG, Text } = widget;
+
+function Widget() {
+  return (
+    <AutoLayout
+      direction="horizontal"
+      horizontalAlignItems="center"
+      verticalAlignItems="center"
+      height="hug-contents"
+      padding={8}
+      fill="#FFFFFF"
+      cornerRadius={8}
+      spacing={12}
+    >
+      <Text fontSize={32} horizontalAlignText="center">
+        Hello Widgets
+      </Text>
+    </AutoLayout>
+  );
+}
+widget.register(Widget);
